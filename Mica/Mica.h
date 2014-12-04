56ece5fcf113ccd3407d39b216602ca453584ce0
Hiseh <hisehyin@gmail.com>
2014-12-04 14:26:26 +0800

    total head file

--- Mica/Mica.h
+++ Mica/Mica.h
@@ -5,21 +5,23 @@
 //  Created by hiseh yin on 14/11/26.
 //  Copyright (c) 2014年 hiseh. All rights reserved.
 //
+#import <Foundation/Foundation.h>
+#import <Availability.h>
 
-#ifndef Mica_Example_Mica_h
-#define Mica_Example_Mica_h
+#ifndef _MICA_
+    #define _MICA_
 
-#import "UIColor+MCColor.h"
-#import "NSString+MCString.h"
-#import "NSNumber+MCNumber.h"
+    #import "UIColor+MCColor.h"
+    #import "NSString+MCString.h"
+    #import "NSNumber+MCNumber.h"
 
-#import "UITextField+MCTextField.h"
-#import "UILabel+MCLabel.h"
-#import "UITextView+MCTextView.h"
-#import "UIView+MCView.h"
+    #import "UITextField+MCTextField.h"
+    #import "UILabel+MCLabel.h"
+    #import "UITextView+MCTextView.h"
+    #import "UIView+MCView.h"
 
-#import "MCView.h"
-#import "MCListView.h"
+    #import "MCView.h"
+    #import "MCListView.h"
 
 #define MC_PARAMETERS_FILE @"Parameters"
 
